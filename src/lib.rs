20713f315e1c73db7791dd5f430c73555f0f651c
Him10Agr <himanshuagr.c2@gmail.com>
2025-03-14 16:01:39 +0530

    created basic function with python macro and python module with rust. Also created basic setup.py configuration file

--- src/lib.rs
+++ src/lib.rs
@@ -2,8 +2,9 @@ use pyo3::prelude::*;
 use pyo3::wrap_pyfunction;
 
 #[pyfunction]
-fn say_hello() {
-    println!("Saying hello from Rust!")
+fn say_hello() -> PyResult<()> {
+    println!("Saying hello from Rust!");
+    return Ok(());
 }
 
 #[pymodule]
