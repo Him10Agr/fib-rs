0291333af4ff0a2d1042d51933cd939d920219a0
Him10Agr <himanshuagr.c2@gmail.com>
2025-03-16 15:15:32 +0530

    modified lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,25 @@
 use pyo3::prelude::*;
 use pyo3::wrap_pyfunction;
 
+///#[pyfunction]
+//fn say_hello() -> PyResult<()> {
+//    println!("Saying hello from Rust!");
+//    return Ok(());
+//}
+
+//#[pymodule]
+//fn fib_rs(_py: Python, m: &Bound<'_, PyModule>) -> PyResult<()> {
+//    m.add_function(wrap_pyfunction!(say_hello,_py)?)?;
+//    return Ok(());
+//}
+
 #[pyfunction]
-fn say_hello() -> PyResult<()> {
-    println!("Saying hello from Rust!");
-    return Ok(());
+fn say_hello(){
+    println!("Saying hello from Rust!!");
 }
 
 #[pymodule]
-fn fib_rs(_py: Python, m: &Bound<'_, PyModule>) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(say_hello,_py)?)?;
+fn fib_rs(_py: Python, m: &Bound<'_,PyModule>) -> PyResult<()> {
+    let _ = m.add_wrapped(wrap_pyfunction!(say_hello));
     return Ok(());
 }
